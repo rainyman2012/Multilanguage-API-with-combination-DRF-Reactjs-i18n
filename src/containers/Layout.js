22a2828705db37e20e2973c8f95caca4e347045f
Ehsan Ahmadi <e.rainyman2012@gmail.com>
2019-09-20 08:37:53 -0700

    create frontend

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -15,15 +15,40 @@ import { connect } from "react-redux";
 import { logout } from "../store/actions/auth";
 
 class CustomLayout extends React.Component {
+  state = { language: "En" };
+
+  handleLanguage = (event, data) => {
+    this.setState({ language: data.text });
+  };
+
   render() {
+    let elements = React.Children.toArray(this.props.children);
+    if (elements.length === 1) {
+      elements = React.cloneElement(elements[0], {
+        language: this.state.language
+      });
+    }
+
+    console.log(elements);
+
     const { authenticated } = this.props;
     return (
       <div>
         <Menu fixed="top" inverted>
           <Container>
+            <Menu.Item header>
+              <Dropdown text="Language">
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={this.handleLanguage} text="Fa" />
+                  <Dropdown.Item onClick={this.handleLanguage} text="En" />
+                </Dropdown.Menu>
+              </Dropdown>
+            </Menu.Item>
+
             <Link to="/">
               <Menu.Item header>Home</Menu.Item>
             </Link>
+
             {authenticated ? (
               <Menu.Item header onClick={() => this.props.logout()}>
                 Logout
@@ -40,70 +65,7 @@ class CustomLayout extends React.Component {
             )}
           </Container>
         </Menu>
-
-        {this.props.children}
-
-        <Segment
-          inverted
-          vertical
-          style={{ margin: "5em 0em 0em", padding: "5em 0em" }}
-        >
-          <Container textAlign="center">
-            <Grid divided inverted stackable>
-              <Grid.Column width={3}>
-                <Header inverted as="h4" content="Group 1" />
-                <List link inverted>
-                  <List.Item as="a">Link One</List.Item>
-                  <List.Item as="a">Link Two</List.Item>
-                  <List.Item as="a">Link Three</List.Item>
-                  <List.Item as="a">Link Four</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column width={3}>
-                <Header inverted as="h4" content="Group 2" />
-                <List link inverted>
-                  <List.Item as="a">Link One</List.Item>
-                  <List.Item as="a">Link Two</List.Item>
-                  <List.Item as="a">Link Three</List.Item>
-                  <List.Item as="a">Link Four</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column width={3}>
-                <Header inverted as="h4" content="Group 3" />
-                <List link inverted>
-                  <List.Item as="a">Link One</List.Item>
-                  <List.Item as="a">Link Two</List.Item>
-                  <List.Item as="a">Link Three</List.Item>
-                  <List.Item as="a">Link Four</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column width={7}>
-                <Header inverted as="h4" content="Footer Header" />
-                <p>
-                  Extra space for a call to action inside the footer that could
-                  help re-engage users.
-                </p>
-              </Grid.Column>
-            </Grid>
-
-            <Divider inverted section />
-            <Image centered size="mini" src="/logo.png" />
-            <List horizontal inverted divided link size="small">
-              <List.Item as="a" href="#">
-                Site Map
-              </List.Item>
-              <List.Item as="a" href="#">
-                Contact Us
-              </List.Item>
-              <List.Item as="a" href="#">
-                Terms and Conditions
-              </List.Item>
-              <List.Item as="a" href="#">
-                Privacy Policy
-              </List.Item>
-            </List>
-          </Container>
-        </Segment>
+        {elements}
       </div>
     );
   }
--- src/routes.js
+++ src/routes.js
@@ -6,11 +6,15 @@ import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import HomepageLayout from "./containers/Home";
 
-const BaseRouter = () => (
+const BaseRouter = props => (
   <Hoc>
-    <Route path="/login" component={Login} />
-    <Route path="/signup" component={Signup} />
-    <Route exact path="/" component={HomepageLayout} />
+    {/* “When you use the component props, the router uses React.createElement
+      to create a new React element from the given component. That means if you
+      provide an inline function to the component attribute, you would create a
+      new component every render. This results in the existing component unmounting
+      and the new component mounting instead of just updating the existing component.” */}
+
+    <Route exact path="/" render={() => <HomepageLayout {...props} />} />
   </Hoc>
 );
 
